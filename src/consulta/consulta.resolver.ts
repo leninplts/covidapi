2cc324295020add9c1147bc2a2226baece3dcfaa
Lenin Jose <paltokj@gmail.com>
2020-09-03 23:07:58 -0500

    added consulta functionality

--- src/consulta/consulta.resolver.ts
+++ src/consulta/consulta.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { ConsultaService } from './consulta.service';
+import { ConsultaEntity } from './consulta.entity';
+import { ConsultaInput } from './consulta.input';
+import { DoctorUserInput } from './doctoruser.input';
 
 @Resolver('Consulta')
-export class ConsultaResolver {}
+export class ConsultaResolver {
+    constructor(private readonly consultaService: ConsultaService) { }
+
+    @Query(() => [ConsultaEntity])
+    async consultas() {
+        return this.consultaService.findAll();
+    }
+
+    @Mutation(() => ConsultaEntity)
+    async createConsulta(@Args('input') input: ConsultaInput) {
+        console.log(input)
+        return await this.consultaService.create(input);
+        // return this.diagnosticoService.createToken(diagnostico);
+    }
+
+    @Query(() => [ConsultaEntity])
+    async misConsultas(@Args('input') input: DoctorUserInput) {
+        return this.consultaService.misConsultas(input);
+    }
+}
